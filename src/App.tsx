711b1ffb548317590ebc60956f8bfc08221c39c0
hide-stream <hidestream28@gmail.com>
2025-01-21 10:24:11 +0900

    型がつくようになった

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { createClient } from '@supabase/supabase-js'
+import { Database } from './database.types'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  const supabase = createClient('http://127.0.0.1:54321', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0')
+  
+  const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
   supabase
   .from('excuse_registrations')
   .select().then((r) => console.log("res", r))
