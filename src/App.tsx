a2a6dfb7661fafa43a585d5a475d3b799746ed48
hide-stream <hidestream28@gmail.com>
2025-02-01 10:23:50 +0900

    tailwindを使えるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './database.types'
@@ -15,15 +13,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">
+    Hello world!24e
+  </h1>
+  <div className="flex">
+    aaaaa
+    </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
